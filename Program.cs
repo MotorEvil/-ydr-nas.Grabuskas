228fca3c657bd07f72070cd8249d0c5f6140870b
Žydrūnas Grabauskas <55529426+MotorEvil@users.noreply.github.com>
2020-09-26 12:03:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Žydrūnas.Grabuskas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
